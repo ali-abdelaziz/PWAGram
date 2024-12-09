d68a5e9b5897fb7f9f1cb2e94b63794849415280
Ali Abdelaziz <alimabdelaziz1@gmail.com>
2024-12-09 12:48:58 +0200

    connect & test chrome dev tools to a real emulated device, defer the app install banner

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,7 +1,14 @@
+var deferredPrompt;
+
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-  .register("/sw.js")
-  .then(function () {
+  navigator.serviceWorker.register("/sw.js").then(function () {
     console.log("Service Worker Registered");
   });
 }
+
+window.addEventListener("beforeinstallprompt", function (event) {
+  console.log("beforeinstallprompt event fired");
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
