11d54a5a6ff1ec4dde67bbb6f9fd75448e113dd8
Ali Abdelaziz <alimabdelaziz1@gmail.com>
2024-12-10 12:14:23 +0200

    example for how to handle promises

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,8 +1,12 @@
 var deferredPrompt;
 
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js").then(function () {
+  navigator.serviceWorker.register("/sw.js")
+  .then(function () {
     console.log("Service Worker Registered");
+  })
+  .catch(function (err) {
+    console.log(err, "Service Worker Registeration Failed");
   });
 }
 
@@ -12,3 +16,37 @@ window.addEventListener("beforeinstallprompt", function (event) {
   deferredPrompt = event;
   return false;
 });
+
+var promise = new Promise (function(resolve, reject) {
+  setTimeout(function() {
+    // resolve('This is excuted once the timer is done');
+    reject({code: 500, message: 'An Error Occured'});
+    // console.log('This is excuted once the timer is done');
+  }, 3000)
+})
+
+// Promises example
+// handle ersolve
+// promise.then(function(text) {
+//   return text;
+// }, function(err) {
+//   console.log(err.code, err.message);
+// })
+// .then(function(newText) {
+//   console.log(newText);
+// });
+
+// handle reject
+promise.then(function(text) {
+  return text;
+}, function(err) {
+  console.log(err.code, err.message);
+})
+.then(function(newText) {
+  console.log(newText);
+})
+.catch(function(err) {
+  console.log(err.code, err.message);
+});
+
+console.log('This is excuted after the timeout');
