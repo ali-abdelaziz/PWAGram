31f06b74b7bd1ff807301cb2b47ba4f973bf3a66
Ali Abdelaziz <alimabdelaziz1@gmail.com>
2024-12-10 14:06:46 +0200

    example for handling fetch & ajax for handling http requests using 'https://httpbin.org', add polyfills files for legacy browser support for using fetch

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,8 @@
 var deferredPrompt;
+// adding polyfill for promise
+if (!window.Promise) {
+  window.Promise = Promise;
+}
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js")
@@ -25,16 +29,73 @@ var promise = new Promise (function(resolve, reject) {
   }, 3000)
 })
 
+// example for using Ajax, We can not use traditional Ajax method with service worker
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://httpbin.org/ip");
+
+xhr.responseType = "json";
+
+xhr.onload = function () {
+  console.log(xhr.response);
+};
+
+xhr.onerror = function () {
+  console.log("Errror");
+};
+
+xhr.send();
+
+// Fetch is the best way to make http request in service worker
+// Fetch
+// get request
+fetch('https://httpbin.org/ip')
+// fetch('https://httpbin.org/ipssss') // wrong url for testing error handling
+.then(function(respone) {
+  console.log(respone);
+  return respone.json();
+})
+.then(function(data) {
+  console.log(data);
+})
+.catch(function(err) {
+  console.log(err);
+});
+
+// post request
+fetch('https://httpbin.org/post', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  },
+  mode: 'cors', // no-cors, cors, same-origin - [ (default) is 'cors' ]
+  // mode: 'no-cors', // no-cors, cors, same-origin - [ (default) is 'cors' ]
+  // body: {body: 'This is a test'}
+  body: JSON.stringify({
+    body: 'This is a test'
+  })
+})
+.then(function(respone) {
+  console.log(respone);
+  return respone.json();
+})
+.then(function(data) {
+  console.log(data);
+})
+.catch(function(err) {
+  console.log(err);
+});
+
 // Promises example
 // handle ersolve
-// promise.then(function(text) {
-//   return text;
-// }, function(err) {
-//   console.log(err.code, err.message);
-// })
-// .then(function(newText) {
-//   console.log(newText);
-// });
+promise.then(function(text) {
+  return text;
+}, function(err) {
+  console.log(err.code, err.message);
+})
+.then(function(newText) {
+  console.log(newText);
+});
 
 // handle reject
 promise.then(function(text) {
