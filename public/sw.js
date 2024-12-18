b4b4e4d6bf9ab3d9c0d48c6af6114f4e1a66406b
Ali Abdelaziz <alimabdelaziz1@gmail.com>
2024-12-18 18:11:41 +0200

    NETWORK WITH CACH FALLBACK STRATEGY

--- public/sw.js
+++ public/sw.js
@@ -81,8 +81,25 @@ self.addEventListener("activate", function (event) {
 // });
 
 // Network only strategy - [ only return data from the network ]
+// self.addEventListener("fetch", function (event) {
+//   event.respondWith(
+//     fetch(event.request)
+//   );
+// });
+
+// Network with cache fallback strategy
 self.addEventListener("fetch", function (event) {
   event.respondWith(
     fetch(event.request)
+    .then(function (res) {
+      return caches.open(CACH_DYNAMIC_NAME)
+      .then(function (cache) {
+        cache.put(event.request.url, res.clone());
+        return res;
+      })
+    })
+    .catch(function (err) {
+      return caches.match(event.request);
+    })
   );
 });
